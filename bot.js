85b3e516a7bdfefc929dd6caa29db3315c18094d
DanielGuardado <59269194+DanielGuardado@users.noreply.github.com>
2020-10-16 18:14:40 -0400

    add welcome message

--- bot.js
+++ bot.js
@@ -1,70 +1,81 @@
 //  __   __  ___        ___
-// |__) /  \  |  |__/ |  |  
-// |__) \__/  |  |  \ |  |  
+// |__) /  \  |  |__/ |  |
+// |__) \__/  |  |  \ |  |
 
 // This is the main file for the iBot bot.
 
 // Import Botkit's core features
-const { Botkit } = require('botkit');
-const { BotkitCMSHelper } = require('botkit-plugin-cms');
+const { Botkit } = require("botkit");
+const { BotkitCMSHelper } = require("botkit-plugin-cms");
 
 // Import a platform-specific adapter for web.
 
-const { WebAdapter } = require('botbuilder-adapter-web');
+const { WebAdapter } = require("botbuilder-adapter-web");
 
-const { MongoDbStorage } = require('botbuilder-storage-mongodb');
+const { MongoDbStorage } = require("botbuilder-storage-mongodb");
 
 // Load process.env values from .env file
-require('dotenv').config();
+require("dotenv").config();
 
 let storage = null;
 if (process.env.MONGO_URI) {
-    storage = mongoStorage = new MongoDbStorage({
-        url : process.env.MONGO_URI,
-    });
+  storage = mongoStorage = new MongoDbStorage({
+    url: process.env.MONGO_URI,
+  });
 }
-
 
 const adapter = new WebAdapter({});
 
-
 const controller = new Botkit({
-    webhook_uri: '/api/messages',
+  webhook_uri: "/api/messages",
 
-    adapter: adapter,
+  adapter: adapter,
 
-    storage
+  storage,
 });
 
 if (process.env.CMS_URI) {
-    controller.usePlugin(new BotkitCMSHelper({
-        uri: process.env.CMS_URI,
-        token: process.env.CMS_TOKEN,
-    }));
+  controller.usePlugin(
+    new BotkitCMSHelper({
+      uri: process.env.CMS_URI,
+      token: process.env.CMS_TOKEN,
+    })
+  );
 }
 
 // Once the bot has booted up its internal services, you can use them to do stuff.
 controller.ready(() => {
+  // load traditional developer-created local custom feature modules
+  controller.loadModules(__dirname + "/features");
 
-    // load traditional developer-created local custom feature modules
-    controller.loadModules(__dirname + '/features');
+  /* catch-all that uses the CMS to trigger dialogs */
+  if (controller.plugins.cms) {
+    // controller.on("conversationStarted", async (bot, message) => {
+    //   await console.log("hi");
+    // });
+    // controller.on("hello")
+    // controller.trigger("welcome");
 
-    /* catch-all that uses the CMS to trigger dialogs */
-    if (controller.plugins.cms) {
-        controller.on('message,direct_message', async (bot, message) => {
-            let results = false;
-            results = await controller.plugins.cms.testTrigger(bot, message);
+    // controller.trigger("my_custom_event", bot, message);
 
-            if (results !== false) {
-                // do not continue middleware!
-                return false;
-            }
-        });
-    }
+    // handle the custom event
+    // controller.on('my_custom_event', async (bot, message) => {
+    //     await bot.beginDialog("welcome");
+    // });
 
+    // controller.on("testing", )
+    // controller.on("hello", async (bot, message) => {
+    //   await bot.beginDialog(convo);
+    // });
+
+    controller.on("message,direct_message", async (bot, message) => {
+      let results = false;
+      results = await controller.plugins.cms.testTrigger(bot, message);
+
+      if (results !== false) {
+        // do not continue middleware!
+        return false;
+      }
+    });
+  }
 });
-
-
-
-
-
---
+++ features/welcome.js
@@ -0,0 +1,74 @@
+const { BotkitConversation } = require("botkit");
+const typing = require("./typing");
+
+module.exports = function (controller) {
+  let convo = new BotkitConversation("welcome", controller);
+  // send a greeting
+  convo.addAction("typing");
+
+  convo.addMessage({ type: "typing" }, "typing");
+
+  convo.addAction("next_thread", "typing");
+
+  convo.say("Hi my name is Daniel!");
+
+  convo.addAction("typing");
+
+  convo.addMessage({ type: "typing" }, "typing");
+
+  convo.addAction("next_thread", "typing");
+
+  convo.addMessage("What would you like to know about me?", "next_thread");
+
+  convo.before("next_thread", async () => {
+    return new Promise((resolve) => {
+      // simulate some long running process
+      setTimeout(resolve, 3000);
+    });
+  });
+
+  controller.addDialog(convo);
+
+  controller.on("hello", async (bot, message) => {
+    await bot.beginDialog("welcome");
+  });
+
+  controller.hears("hello", "message", async (bot, message) => {
+    await bot.beginDialog("welcome");
+  });
+};
+
+// let iBot = new BotkitConversation("welcome", controller);
+
+// iBot.say({ type: "typing" });
+// iBot.addAction("welcome");
+
+// iBot.addMessage("Hello i am iBot", "next_action");
+// iBot.addAction("next_action", "welcome");
+// iBot.addMessage({ type: "typing" }, "next_action");
+// iBot.addAction("last_thread", "next_action");
+
+// iBot.addMessage({ type: "typing" }, "welcome");
+
+// iBot.addMessage(
+//   {
+//     text: "What you want b",
+//     quick_replies: "hi",
+//   },
+//   "last_thread"
+// );
+
+// iBot.before("next_thread", async () => {
+//   return new Promise((resolve) => {
+//     setTimeout(resolve, 1500);
+//   });
+// });
+// iBot.before("last_thread", async () => {
+//   return new Promise((resolve) => {
+//     setTimeout(resolve, 1500);
+//   });
+// });
+// controller.on("testing", async (bot, message) => {
+//   // await bot.beginDialog("welcome");
+//   bot.say("hello");
+// });
