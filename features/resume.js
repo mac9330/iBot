d98856cdc5943d7b208490879c24ccc1ab94ffea
Anthony-E-Cruz <anthonyecruz@gmail.com>
2020-10-18 16:37:36 -0400

    welcome messages send with pause in between

---
+++ features/resume.js
@@ -0,0 +1,34 @@
+
+module.exports = function ( controller ) {
+  controller.resume = {
+    work: {
+      riteAid : {
+        name: "Rite Aid",
+        title: "Assisstant Manager",
+        description: "DO things",
+        timeline: "2013-2020"
+      }
+    },
+    education: {
+      appAcademy: {
+        name: "App Academy",
+        description: "We did things",
+        timeline: "April 2020 - July 2020"
+      }
+    },
+    techStack: {
+      languages: {
+        name: "Ruby and JavaScript"
+      }
+      // otherTechnology: {
+
+      // }
+    },
+    contact: {
+      email: "danguardado217@gmail.com",
+      phone: "(631)552-8552",
+      social: "https://www.linkedin.com/in/danielguardado1/"
+    }
+    
+  }
+}
--- features/welcome.js
+++ features/welcome.js
@@ -2,29 +2,40 @@ const { BotkitConversation } = require("botkit");
 const typing = require("./typing");
 
 module.exports = function (controller) {
+  const replies = [
+    {
+      title: 'Hello',
+      payload: 'hello'
+    },
+    {
+      title: 'Help',
+      payload: 'help'
+    },
+  ]
+
   let convo = new BotkitConversation("welcome", controller);
-  // send a greeting
-  convo.addAction("typing");
 
-  convo.addMessage({ type: "typing" }, "typing");
-
-  convo.addAction("next_thread", "typing");
-
-  convo.say("Hi my name is Daniel!");
-
-  convo.addAction("typing");
-
-  convo.addMessage({ type: "typing" }, "typing");
-
-  convo.addAction("next_thread", "typing");
-
-  convo.addMessage("What would you like to know about me?", "next_thread");
+  convo.say({ type: "typing" })
+  convo.addAction("welcome")
+  convo.addMessage("Hi my name is Daniel!", "next_thread")
+  convo.addAction("next_thread", "welcome")
+  convo.addMessage({ type: "typing" }, "next_thread")
+  convo.addAction("last_thread", "next_thread")
+  convo.addMessage({ type: "typing" }, "welcome")
+  convo.addMessage({
+    text: "what do you want to know?", quick_replies: replies
+  }, "last_thread")
 
   convo.before("next_thread", async () => {
     return new Promise((resolve) => {
-      // simulate some long running process
-      setTimeout(resolve, 3000);
-    });
+      setTimeout(resolve, 1500)
+    })
+  });
+
+  convo.before("last_thread", async () => {
+    return new Promise((resolve) => {
+      setTimeout(resolve, 1500)
+    })
   });
 
   controller.addDialog(convo);
@@ -33,42 +44,5 @@ module.exports = function (controller) {
     await bot.beginDialog("welcome");
   });
 
-  controller.hears("hello", "message", async (bot, message) => {
-    await bot.beginDialog("welcome");
-  });
-};
+}
 
-// let iBot = new BotkitConversation("welcome", controller);
-
-// iBot.say({ type: "typing" });
-// iBot.addAction("welcome");
-
-// iBot.addMessage("Hello i am iBot", "next_action");
-// iBot.addAction("next_action", "welcome");
-// iBot.addMessage({ type: "typing" }, "next_action");
-// iBot.addAction("last_thread", "next_action");
-
-// iBot.addMessage({ type: "typing" }, "welcome");
-
-// iBot.addMessage(
-//   {
-//     text: "What you want b",
-//     quick_replies: "hi",
-//   },
-//   "last_thread"
-// );
-
-// iBot.before("next_thread", async () => {
-//   return new Promise((resolve) => {
-//     setTimeout(resolve, 1500);
-//   });
-// });
-// iBot.before("last_thread", async () => {
-//   return new Promise((resolve) => {
-//     setTimeout(resolve, 1500);
-//   });
-// });
-// controller.on("testing", async (bot, message) => {
-//   // await bot.beginDialog("welcome");
-//   bot.say("hello");
-// });
